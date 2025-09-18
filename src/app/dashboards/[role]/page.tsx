cd6436c77c884d3b55559d245dd6855557c410fa
Yusuf Çimen <47089035+yusufcimenn@users.noreply.github.com>
2025-09-18 14:58:16 +0300

    infrastructure update

--- src/app/dashboards/[role]/page.tsx
+++ src/app/dashboards/[role]/page.tsx
@@ -5,7 +5,7 @@ export default function RoleHome({ params }: { params: { role: Role } }) {
   return (
     <section className="rounded-2xl bg-white p-4 shadow">
       <h2 className="font-semibold mb-2">Hoş geldiniz</h2>
-      <p className="text-sm text-neutral-600">Bu sayfa {params.role} rolü için ana ekrandır.</p>
+      <p className="text-sm text-neutral-600">This page  {params.role} roles main page..</p>
     </section>
   );
 }
